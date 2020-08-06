e479330f7c23a0c5be34f3e7c2eaa58466680a3f
Adrian Smith <sfyire@gmail.com>
2020-08-06 22:40:52 +0100

    WIP

--- src/slifin/beeline.php
+++ src/slifin/beeline.php
@@ -5,9 +5,32 @@
  * Beeline interface for PHP.
  */
 
-namespace slifin;
+namespace slifin\beeline;
 
-class Beeline
+function _send(...$arguments)
 {
+     // https://stackoverflow.com/a/2390755/978970
+    $jar = sprintf('%s/jars/honeysql-1.0.444.jar', __DIR__);
+    $command = sprintf('bb -f beeline.clj -cp %s', $jar);
 
+    $descriptorspec = [
+       0 => ['pipe', 'r'],
+       1 => ["pipe", "w"],
+    ];
+
+    $process = proc_open($command, $descriptorspec, $pipes);
+
+    if (!is_resource($process)) {
+        throw new Exception('Failed to open process');
+    }
+
+    [$stdin, $stdout] = $pipes;
+    fwrite($stdin, $arguments);
+    fclose($stdin);
+
+    $query = stream_get_contents($stdout);
+    fclose($stdout);
+    $return_value = proc_close($process);
+
+    return $query;
 }
