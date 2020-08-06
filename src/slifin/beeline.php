aa1cd722554b4559836a323c86492455b3016539
Adrian Smith <sfyire@gmail.com>
2020-08-06 22:45:22 +0100

    WIP

--- src/slifin/beeline.php
+++ src/slifin/beeline.php
@@ -7,18 +7,17 @@
 
 namespace slifin\beeline;
 
-function _send(...$arguments)
+function bridge(...$arguments)
 {
-     // https://stackoverflow.com/a/2390755/978970
     $jar = sprintf('%s/jars/honeysql-1.0.444.jar', __DIR__);
     $command = sprintf('bb -f beeline.clj -cp %s', $jar);
 
-    $descriptorspec = [
+    $descriptor = [
        0 => ['pipe', 'r'],
        1 => ["pipe", "w"],
     ];
 
-    $process = proc_open($command, $descriptorspec, $pipes);
+    $process = proc_open($command, $descriptor, $pipes);
 
     if (!is_resource($process)) {
         throw new Exception('Failed to open process');
