2b95b686477d4da5f5f64e98b1f029dee65572da
Adrian Smith <sfyire@gmail.com>
2020-08-09 16:59:29 +0100

    3 lines of clojure

--- src/slifin/beeline.clj
+++ src/slifin/beeline.clj
@@ -2,13 +2,6 @@
   (:require [honeysql.core :as sql]
             [cognitect.transit :as transit]))
 
-
-(def input (-> System/in
-               (transit/reader :json)
-               transit/read))
-
-(def result (->> input (apply sql/format)))
-
-(def writer (transit/writer System/out :json))
-
-(transit/write writer result)
+(->> (-> System/in (transit/reader :json) transit/read)
+     (apply sql/format)
+     (transit/write (transit/writer System/out :json)))
